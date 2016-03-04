4c33369434423f396848e4fd4b9e65ef4b1425c8
Shawn Lunny <shawn.lunny@shaleplaysmedia.com>
2016-03-04 02:13:17 -0600

    Submit Assignment 2 along with a test file that will verify our functions work correctly

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The point of the functions is to reduce the processing burden while inverting a matrix
+## especially if the same inversion has already occurred previously
+## The cache matrix funtion declares the matrix object and the cache solve matrix sets the data in the matrix
 
-## Write a short comment describing this function
+
+## Make an object function that has setters and getters for the data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inverse <<- NULL
+  
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function(){
+    x
+  }
+  
+  setinverse <- function(solve){
+    inverse <<- solve
+  }
+  
+  getinverse <- function(){
+    inverse
+  }
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x' if the matrix is a square
+## Note: If the user does not use a square matrix the system will throw an error on solve
+## if there is not a second matrix provided, thus we include the '...' if it is needed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getinverse()
+  
+  if(!is.null(inverse)){
+    message("Getting cached matrix")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  
+  inverse <- solve(data, ...)
+  
+  x$setinverse(inverse)
+  
+  inverse
 }
---
+++ testcachematrix.R
@@ -0,0 +1,43 @@
+##Lets test if the caching function is actually caching
+
+##Learned to time functions from this stackoverflow post
+##http://stackoverflow.com/questions/6262203/measuring-function-execution-time-in-r
+
+#make sure we load our functions file
+source("cachematrix.R")
+
+message("Setting seed to 800 so we can fix our output to be the same for comparison")
+set.seed(800)
+#make matrix 1000 x 1000  with 100 million observations on a distribution from 1 to 1000
+testmatrix <- matrix(runif(10000^2, 1, 1000), 1000, 1000)
+
+#make the cache matrix object
+testcachematrix <- makeCacheMatrix(testmatrix)
+
+message("Get the time of our solved matrix object")
+print(system.time(cachematrix1 <- cacheSolve(testcachematrix)))
+
+message("Get the time of our solved matrix object (cached)")
+print(system.time(cachematrix2 <- cacheSolve(testcachematrix)))
+
+message("Is our 1st and 2nd matrix the same?")
+print(identical(cachematrix1, cachematrix2))
+
+message("Setting seed to 900 so we can fix our output to be the same for comparison")
+set.seed(900)
+#make matrix 1000 x 1000  with 100 million observations on a distribution from 1 to 1000
+testmatrix <- matrix(runif(10000^2, 1, 1000), 1000, 1000)
+
+message("Setting the new matrix object to override the cached version")
+testcachematrix$set(testmatrix)
+message("Get the time of our solved cached matrix object")
+print(system.time(cachematrix3 <- cacheSolve(testcachematrix)))
+
+message("Did our new seed give us different matricies? We should see false...")
+print(identical(cachematrix1, cachematrix3))
+
+message("Get the time of our solved matrix object (cached)")
+print(system.time(cachematrix4 <- cacheSolve(testcachematrix)))
+
+message("Is our 3rd and 4th matrix the same?")
+print(identical(cachematrix3, cachematrix4))
