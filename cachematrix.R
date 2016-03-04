34ef5fc7eeb75941f553538786e055e420888c3e
Shawn Lunny <shawn.lunny@shaleplaysmedia.com>
2016-03-04 02:33:58 -0600

    Include additional comments in the code

--- cachematrix.R
+++ cachematrix.R
@@ -9,45 +9,57 @@ makeCacheMatrix <- function(x = matrix()) {
   
   inverse <<- NULL
   
+  #sets the matrix
   set <- function(y){
     x <<- y
     inverse <<- NULL
   }
   
+  #gets the matrix
   get <- function(){
     x
   }
   
+  #inverses the matrix
   setinverse <- function(solve){
     inverse <<- solve
   }
   
+  #retrieves the inverse of the matrix
   getinverse <- function(){
     inverse
   }
   
+  #list the functions of the makeCacheMatrix funtion
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Return a matrix that is the inverse of 'x' if the matrix is a square
-## Note: If the user does not use a square matrix the system will throw an error on solve
-## if there is not a second matrix provided, thus we include the '...' if it is needed
+## Note: If the user does not use a square matrix the system will throw an error 
+## on solve if there is not a second matrix provided, thus we include the '...' 
+## as a parameter forward to solve incase it is needed
 
 cacheSolve <- function(x, ...) {
-  
+  #this will be null the first time the function is run
+  #but will have the inversed matrix on subsequent runs
   inverse <- x$getinverse()
   
+  #if we already inversed this matrix the return it
   if(!is.null(inverse)){
     message("Getting cached matrix")
     return(inverse)
   }
   
+  #otherwise get the matrix
   data <- x$get()
   
+  #inverse the data
   inverse <- solve(data, ...)
   
+  #set the cache with the inversed data
   x$setinverse(inverse)
   
+  #return the inversed matrix
   inverse
 }
--- testcachematrix.R
+++ testcachematrix.R
@@ -14,10 +14,10 @@ testmatrix <- matrix(runif(10000^2, 1, 1000), 1000, 1000)
 #make the cache matrix object
 testcachematrix <- makeCacheMatrix(testmatrix)
 
-message("Get the time of our solved matrix object")
+message("Get the time of our inversed matrix object")
 print(system.time(cachematrix1 <- cacheSolve(testcachematrix)))
 
-message("Get the time of our solved matrix object (cached)")
+message("Get the time of our inversed matrix object (cached)")
 print(system.time(cachematrix2 <- cacheSolve(testcachematrix)))
 
 message("Is our 1st and 2nd matrix the same?")
@@ -30,13 +30,13 @@ testmatrix <- matrix(runif(10000^2, 1, 1000), 1000, 1000)
 
 message("Setting the new matrix object to override the cached version")
 testcachematrix$set(testmatrix)
-message("Get the time of our solved cached matrix object")
+message("Get the time of our inversed cached matrix object")
 print(system.time(cachematrix3 <- cacheSolve(testcachematrix)))
 
 message("Did our new seed give us different matricies? We should see false...")
 print(identical(cachematrix1, cachematrix3))
 
-message("Get the time of our solved matrix object (cached)")
+message("Get the time of our inversed matrix object (cached)")
 print(system.time(cachematrix4 <- cacheSolve(testcachematrix)))
 
 message("Is our 3rd and 4th matrix the same?")
